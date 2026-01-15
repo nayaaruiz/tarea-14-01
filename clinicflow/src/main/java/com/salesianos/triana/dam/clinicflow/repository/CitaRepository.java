25174e579c314c2926b4ee3c4d5b29668e0893c5
nayaaruiz <nayaaaruiz@gmail.com>
2026-01-16 00:48:18 +0100

    night

--- clinicflow/src/main/java/com/salesianos/triana/dam/clinicflow/repository/CitaRepository.java
+++ clinicflow/src/main/java/com/salesianos/triana/dam/clinicflow/repository/CitaRepository.java
@@ -1,9 +1,31 @@
 package com.salesianos.triana.dam.clinicflow.repository;
 
 import com.salesianos.triana.dam.clinicflow.model.Cita;
+import com.salesianos.triana.dam.clinicflow.model.Estado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface CitaRepository extends JpaRepository <Cita, Long> {
+
+    List<Cita> findByPacienteId(Long paciente_id);
+
+    Page<Cita> findByEstado(Estado estado, Pageable pageable);
+
+    boolean exististsByProfesionalIdAndFechaHora(
+            Long profesional_id,
+            LocalDateTime fechaHora
+    );
+
+    boolean existsByPacienteIdAndFechaHoraBetween(
+            Long paciente_id,
+            LocalDateTime inicio,
+            LocalDateTime fin
+    )
+
 }
--- clinicflow/src/main/java/com/salesianos/triana/dam/clinicflow/service/CitaService.java
+++ clinicflow/src/main/java/com/salesianos/triana/dam/clinicflow/service/CitaService.java
@@ -1,26 +1,91 @@
 package com.salesianos.triana.dam.clinicflow.service;
 
-import com.salesianos.triana.dam.clinicflow.model.Cita;
+import com.salesianos.triana.dam.clinicflow.dto.CreateCitaRequest;
+import com.salesianos.triana.dam.clinicflow.dto.CreateConsultaRequest;
+import com.salesianos.triana.dam.clinicflow.model.*;
 import com.salesianos.triana.dam.clinicflow.repository.CitaRepository;
+import com.salesianos.triana.dam.clinicflow.repository.PacienteRepository;
+import com.salesianos.triana.dam.clinicflow.repository.ProfesionalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CitaService {
 
-    private final CitaRepository repository;
+    private final CitaRepository citaRepository;
+    private final PacienteRepository pacienteRepository;
+    private final ProfesionalRepository profesionalRepository;
 
-    public List<Cita> findAll() { return repository.findAll(); }
+    public void crearCita(CreateCitaRequest request) {
+        if (request.fechaHora().isBefore(LocalDateTime.now())){
+            throw new RuntimeException("No se puede crear una cita en el pasado");
+        }
 
-    public void crearCita(Cita cita) {
-        repository.save(cita);
+        if (citaRepository.exististsByProfesionalIdAndFechaHora(
+                request.profesional_id(),
+                request.fechaHora())) {
+            throw new RuntimeException("Profesional ocupado");
+        }
+
+        //Sólo una cita al día
+        LocalDate dia = request.fechaHora().toLocalDate();
+        if (citaRepository.existsByPacienteIdAndFechaHoraBetween(
+                request.paciente_id(),
+                dia.atStartOfDay(),
+                dia.atTime(LocalTime.MAX))){
+            throw new RuntimeException("El paciente ya tiene una cita ese día");
+        }
+
+        Paciente paciente = pacienteRepository
+                .findById(request.paciente_id())
+                .orElseThrow();
+
+        Profesional profesional = profesionalRepository
+                .findById(request.profesional_id())
+                .orElseThrow();
+
+        Cita cita = new Cita();
+        cita.setFechaHora(request.fechaHora());
+        cita.setEstado(Estado.PROGRAMADA);
+        cita.setPaciente(paciente);
+        cita.setProfesional(profesional);
+
+        citaRepository.save(cita);
+
     }
 
-    public void cancelarCita(Cita cita) {
-        repository.delete(cita);
+    public void cancelarCita(Long id) {
+
+        Cita cita = citaRepository.findById().orElseThrow();
+
+        if (cita.getEstado() == Estado.ATENDIDA) {
+            throw new RuntimeException("No se puede cancelar una cita atendida");
+        }
+
+        cita.setEstado(Estado.CANCELADA);
+
+    }
+
+    public void registrarConsulta (Long id, CreateConsultaRequest request) {
+
+        Cita cita = citaRepository.findById(id).orElseThrow();
+
+        if (cita.getEstado() != Estado.PROGRAMADA) {
+            throw new RuntimeException("La cita no está programada");
+        }
+
+        Consulta consulta = new Consulta();
+        consulta.setFecha(LocalDate.now());
+        consulta.setObservaciones(request.observaciones());
+        consulta.setDiagnostico(request.diagnostico());
+        consulta.set
+
     }
 
 }
